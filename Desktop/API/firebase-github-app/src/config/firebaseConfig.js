0749b5e3600a213c7d14da990ab4122925c3f645
Aatir <aatirqureshi1@gmail.com>
2020-09-23 12:54:02 +0530

    Completed Api with github

---
+++ Desktop/API/firebase-github-app/src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAK-JFB_v2gwr0kEVO0OdjSYTKm5Nv23rY",
+    authDomain: "githubsearchappusingfirebase.firebaseapp.com",
+    databaseURL: "https://githubsearchappusingfirebase.firebaseio.com",
+    projectId: "githubsearchappusingfirebase",
+    storageBucket: "githubsearchappusingfirebase.appspot.com",
+    messagingSenderId: "778562399762",
+    appId: "1:778562399762:web:4c94ab23a006ceada9b2cb"
+  };
